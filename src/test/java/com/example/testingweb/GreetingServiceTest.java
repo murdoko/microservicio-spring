30e9664651fe52abc75c4cd625a97d762ad40798
murdoko <murdoko@gmail.com>
2023-09-22 15:42:57 -0300

    Update GreetingServiceTest.java

--- src/test/java/com/example/testingweb/GreetingServiceTest.java
+++ src/test/java/com/example/testingweb/GreetingServiceTest.java
@@ -2,10 +2,8 @@ package com.example.testingweb;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+
+
 import org.springframework.test.web.servlet.MockMvc;
 
 import org.junit.jupiter.api.Test;
@@ -24,16 +22,9 @@ public class GreetingServiceTest {
 
 	
 	    @Test
-	    @PrepareForTest(SpringApplication.class)
 	    public void mainPassesArguments() {
 	        mockStatic(SpringApplication.class);
-	
 	        TestingWebApplication.main(new String[]{"Hello", "World"});
-	
-	        // start verifying behavior
-	        PowerMockito.verifyStatic();
-	        // call the static method you want to verify
-	        SpringApplication.run(TestingWebApplication.class, new String[]{"Hello", "World"});
 	    }
     
 		
