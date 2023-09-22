6935d3e2897d13057df4721a8e44ecbe79a34624
murdoko <murdoko@gmail.com>
2023-09-22 13:15:23 -0300

    Update GreetingServiceTest.java

--- src/test/java/com/example/testingweb/GreetingServiceTest.java
+++ src/test/java/com/example/testingweb/GreetingServiceTest.java
@@ -1,14 +1,14 @@
 package com.example.testingweb;
 
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class GreetingServiceTest {
 
 	@Test
 	public void test1() {
 		GreetingService greetingService = new GreetingService();
-		assertEquals("Hello, World", "Hello, World");
+		assertThat("Hello, World", "Hello, World");
 	
 	}
 }
