1b6b99266bc777fe82f76944dc57fda5fa76c681
murdoko <murdoko@gmail.com>
2023-09-22 12:44:31 -0300

    Update newTest.java

--- src/test/java/com/example/testingweb/newTest.java
+++ src/test/java/com/example/testingweb/newTest.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class SmokeTest {
+public class GreetingServiceTest {
 
 	@Autowired
 	private GreetingService service;
