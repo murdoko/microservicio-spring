7ec0d72149a97f201668096aefb3a5e74ffa82e8
murdoko <murdoko@gmail.com>
2023-09-22 18:00:55 -0300

    Update GreetingServiceTest.java

--- src/test/java/com/example/testingweb/GreetingServiceTest.java
+++ src/test/java/com/example/testingweb/GreetingServiceTest.java
@@ -16,11 +16,10 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 
-import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest
 public class GreetingServiceTest {
 
