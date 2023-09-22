af3e0030df1c7f7b5ad40f8d5c6cae30e4dfd923
murdoko <murdoko@gmail.com>
2023-09-22 15:19:47 -0300

    Update TestingWebApplication.java

--- src/main/java/com/example/testingweb/TestingWebApplication.java
+++ src/main/java/com/example/testingweb/TestingWebApplication.java
@@ -8,6 +8,10 @@ public class TestingWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestingWebApplication.class, args);
-		System.out.println('ok');
+		System.out.println(printMessage1("Ok"));
 	}
+
+	static String printMessage1(String value) {
+            return value;
+    }
 }
