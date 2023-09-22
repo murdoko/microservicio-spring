ba0eb4d500c2fa067ce6104a2e8e92cdb7d51557
murdoko <murdoko@gmail.com>
2023-09-22 14:31:09 -0300

    Update GreetingServiceTest.java

--- src/test/java/com/example/testingweb/GreetingServiceTest.java
+++ src/test/java/com/example/testingweb/GreetingServiceTest.java
@@ -2,6 +2,10 @@ package com.example.testingweb;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 public class GreetingServiceTest {
