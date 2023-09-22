a61987ba85d15629d00073b606a3ba5eb2ad57c2
murdoko <murdoko@gmail.com>
2023-09-22 13:06:30 -0300

    Update GreetingServiceTest.java

--- src/test/java/com/example/testingweb/GreetingServiceTest.java
+++ src/test/java/com/example/testingweb/GreetingServiceTest.java
@@ -1,22 +1,14 @@
 package com.example.testingweb;
 
 import org.junit.jupiter.api.Test;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertEquals;
+import org.assertj.core.api.Assertions
 
 public class GreetingServiceTest {
 
 	@Test
 	public void test1() {
 		GreetingService greetingService = new GreetingService();
-		final greet resultado = greetingService.greet();
+		final resultado = greetingService.greet();
 		Assertions.assertEquals("Hello, World", resultado);
 		
 	}
